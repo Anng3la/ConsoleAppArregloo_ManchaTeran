e583937b078c49ad841bc83cdd00cffdf62cbb2a
Angela <Angela@LAPTOP-50BD6CII>
2021-10-19 13:57:20 -0500

    Add project files.

---
+++ ConsoleAppArregloo_ManchaTeran/Alumno.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppArregloo_ManchaTeran
+{
+    class Alumno
+    {
+        private string nombre;
+        private string matricula;
+        private string carrera;
+
+        public string Nombre
+        {
+            get
+            {
+                return nombre;
+            }
+            set
+            {
+                nombre = value;
+            }
+        }
+
+        public string Matriula
+        {
+            get
+            {
+                return matricula;
+            }
+            set
+            {
+                matricula = value;
+            }
+        }
+
+        public string Carrera
+        {
+            get
+            {
+                return carrera;
+            }
+            set
+            {
+                carrera = value;
+            }
+        }
+
+        public Alumno()
+        {
+            nombre = "";
+            matricula = "";
+            carrera = "";
+        }
+
+        public Alumno(string nombre, string matricula, string carrera)
+        {
+            this.nombre = nombre;
+            this.matricula = matricula;
+            this.carrera = carrera;
+        }
+
+        public override string ToString()
+        {
+            return "  Nombre: " + nombre + "  Matricula: " + matricula + "  Carrera: " + carrera;
+        }
+    }
+}
---
+++ ConsoleAppArregloo_ManchaTeran/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppArregloo_ManchaTeran
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Alumno[] a = new Alumno[5];
+            for(int i =0; i < a.Length; i++)
+            {
+                a[i] = new Alumno();
+                a[i].Nombre = "";
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine(" - NOMBRE :");
+                a[i].Nombre = Console.ReadLine();
+
+                a[i].Matriula = "";
+                Console.WriteLine(" - MATRICULA :");
+                a[i].Matriula = Console.ReadLine();
+
+                a[i].Carrera = "";
+                Console.WriteLine(" - CARRERA :");
+                a[i].Carrera = Console.ReadLine();
+            }
+
+            for(int i = 0; i < a.Length; i++)
+            {
+                Console.WriteLine(a[i].ToString());
+            }
+            Console.ReadKey();
+        }
+    }
+}
